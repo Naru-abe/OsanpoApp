fb5f1df93af2a94153d3dded8ddd18377db47874
Narumi Abe <smile.narukkory@gmail.com>
2022-08-04 00:33:32 +0000

    [Finish]コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,35 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  # 会員側
+  scope module: :public do
+    get '/' => 'homes#top'
+    get 'about' => 'homes#about'
+    resources :posts do
+      resource :favorite, only: [:create, :destroy]
+      resources :post_comment, only: [:create, :destroy]
+    end
+    resources :end_users, only: [:index, :show, :edit, :update]
+    get 'end_users/unsubscribe' => 'end_users#unsubscribe'
+    patch 'end_users/withdraw' => 'end_users#withdraw'
+  end
+  
+  # 管理者側
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :posts, only: [:index, :show, :destroy] do
+      resources :post_comments, only: [:index, :show, :destroy]
+      resources :tags, except: [:new]
+    end
+    resources :end_users, only: [:index, :show, :edit, :update]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
