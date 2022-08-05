e2b6c37305015d5e89804a4a281284b64f22b01b
Narumi Abe <smile.narukkory@gmail.com>
2022-08-05 05:32:10 +0000

    [Add]投稿機能(一時コミット)

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,22 +1,37 @@
 class Public::PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.end_user_id = current_end_user.id
+    @post.save
+    redirect_to post_path(@post)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:post_image, :content, :tag_id, :station_name, :address, :latitude, :longitude)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ApplicationRecord
+  belongs_to :end_user
+  belongs_to :tga, optional: true
+  has_one_attached :post_image
+
+  def get_post_image
+    (post_image.attached?) ? post_image: 'default-image.jpeg'
+  end
+
 end
