6a69443e8a3fa807c8b17e1da8262b3290e8e08f
Narumi Abe <smile.narukkory@gmail.com>
2022-09-19 09:05:25 +0000

    [Fix]searches_controllerインデント

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,15 +1,15 @@
 class Public::SearchesController < ApplicationController
   before_action :authenticate_end_user!
 
-	def search
-		@model = params[:model]
-		@content = params[:content]
-		end_users = EndUser.where(is_deleted: false)
-		if @model == 'end_user'
-			@records = end_users.search_for(@content, @method).page(params[:page])
-		else
-			@posts = Post.where(end_user: end_users).page(params[:page])
-			@records = @posts.search_for(@content, @method).page(params[:page])
-		end
-	end
+  def search
+    @model = params[:model]
+    @content = params[:content]
+    end_users = EndUser.where(is_deleted: false)
+    if @model == 'end_user'
+      @records = end_users.search_for(@content, @method).page(params[:page])
+    else
+      @posts = Post.where(end_user: end_users).page(params[:page])
+      @records = @posts.search_for(@content, @method).page(params[:page])
+    end
+  end
 end
